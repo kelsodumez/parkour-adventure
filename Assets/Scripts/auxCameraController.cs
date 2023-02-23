b5adce47f5f3a14761df49e7bbd995eaeacd1a17
Kelso Du Mez <kdu40@uclive.ac.nz>
2023-02-23 14:19:18 +1300

    nearly finished #1

---
+++ Assets/Scripts/auxCameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auxCameraController : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+		anim.Play("Establishingcam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
